6e25c0dfc2de3626ed1b366d912e60ae6e3c941c
DhruvGodambe <dhruvrockstone@gmail.com>
2021-03-24 21:48:53 +0530

    made responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,10 @@
-console.log("index.js connected!");
+function showDropdown() {
+    const displayEle  = document.getElementsByClassName("mobile-list")[0];
+    const style = getComputedStyle(displayEle);
+    console.log(typeof(style.height))
+    if( parseInt(style.height) == 0) {
+        displayEle.style.height = "auto"
+    } else {
+        displayEle.style.height = "0"
+    }
+}
